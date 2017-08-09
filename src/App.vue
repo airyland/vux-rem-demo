0119b021ebe8a227db0de9883b8b117409d22eb0
lb <54_leibo@sina.com>
2017-08-09 10:12:57 +0800

    submit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <x-header>798</x-header>
+    <alert v-model="show" title="测试标题" content="测试内容"></alert>
+    <img src="assets/logo.png">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import { XHeader, Alert } from 'vux'
+
+  export default {
+    name: 'app',
+    data () {
+      return {
+        show: true
+      }
+    },
+    components: {
+      XHeader,
+      Alert
+    }
+  }
+</script>
+
+<style>
+  body {
+    margin: 0;
+  }
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+</style>
